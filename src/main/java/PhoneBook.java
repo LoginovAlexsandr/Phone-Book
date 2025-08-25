6c862f5eda5c55fecf99f011d3fb26daccc37e64
Aleksandr Loginov <aloginov527782@gmail.com>
2025-08-25 21:18:36 +0500

    findByNumber implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,13 +1,19 @@
-import java.util.TreeMap;
+import java.util.*;
 
 public class PhoneBook {
     private final TreeMap<String, String> nameToNumber = new TreeMap<>();
+    private final Map<String, String> numberToName = new HashMap<>();
 
     public int add(String name, String number) {
         if (nameToNumber.containsKey(name)) {
             return nameToNumber.size();
         }
         nameToNumber.put(name, number);
+        numberToName.put(number, name);
         return nameToNumber.size();
+    }
+
+    public String findByNumber(String number) {
+        return numberToName.get(number);
     }
 }
