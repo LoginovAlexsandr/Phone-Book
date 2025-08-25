41183ea783a1035af7f8ec459d1a373f55f7f4ca
Aleksandr Loginov <aloginov527782@gmail.com>
2025-08-25 21:13:32 +0500

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,13 @@
+import java.util.TreeMap;
+
 public class PhoneBook {
+    private final TreeMap<String, String> nameToNumber = new TreeMap<>();
+
     public int add(String name, String number) {
-        return 0;
+        if (nameToNumber.containsKey(name)) {
+            return nameToNumber.size();
+        }
+        nameToNumber.put(name, number);
+        return nameToNumber.size();
     }
 }
